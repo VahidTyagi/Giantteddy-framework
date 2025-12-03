5d01de7ee7f4ab0a6fb542bb2668671d8855b74c
Vahid Tyagi <vahidtyagi007@gmail.com>
2025-12-03 17:27:11 +0530

    first commit

---
+++ src/main/java/com/giantteddy/reports/ExtentTestListener.java
@@ -0,0 +1,46 @@
+package com.giantteddy.reports;
+
+import com.aventstack.extentreports.Status;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ExtentTestListener implements ITestListener {
+
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("Starting test suite: " + context.getName());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("Finished test suite: " + context.getName());
+        ExtentManager.getInstance().flush(); // Write report to file
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        String testName = result.getMethod().getMethodName();
+        ExtentTestManager.startTest(testName, "Test execution for " + testName + " on https://giantteddy.com");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        ExtentTestManager.getTest().log(Status.PASS, "Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ExtentTestManager.getTest().log(Status.SKIP, "Test Skipped: " + result.getThrowable());
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        // Optional
+    }
+}
